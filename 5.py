1c4e788e4566c09d70c282a489bdbde2d6ff2125
ELIZA <rysaeva96@mail.ru>
2022-07-24 14:06:54 +0500

    5

--- 5.py
+++ 5.py
@@ -4,11 +4,15 @@
 # для k = 8 список будет выглядеть так: 
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-n = int(input('Введите число: '))
+def fib(n):
+    
+    if n in [1,2]:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+n = int(input("Введите число: "))
+numbers = []
+for e in range(1, n+1):
+    numbers.append(fib(e))
+print(numbers)
 
-fib1 = 1
-fib2 = 1
-print(fib1, fib2, end=' ')
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2 
-    print(fib2, end=' ')
